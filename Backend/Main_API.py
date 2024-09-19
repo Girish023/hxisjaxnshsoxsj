c0b6feb9a60892b58088ee27f1855b845e94518b
Girish Chandra Saxena <130221358+Girish023@users.noreply.github.com>
2024-09-19 10:21:28 +0530

    Update Main_API.py

--- Backend/Main_API.py
+++ Backend/Main_API.py
@@ -4,17 +4,23 @@ from FDA_Home_Page_API import api_home_bp
 from FDA_Compare_API import api_compare_bp
 from FDA_Notification_API import api_notification_bp
 from FDA_Recent_Changes_API import api_recent_changes_bp
+from FDA_Dates_API import api_recent_dates_bp
+from FDA_ASK_API import api_ask_bp
+from FDA_SQL_API import api_sql_bp
 from flask_cors import CORS
-import os
+
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000"])
+CORS(app)
 
 # Register Blueprints
 app.register_blueprint(api_home_bp, url_prefix='/home')
 app.register_blueprint(api_compare_bp, url_prefix='/compare')
 app.register_blueprint(api_notification_bp, url_prefix='/notification')
 app.register_blueprint(api_recent_changes_bp, url_prefix='/recent_changes')
+app.register_blueprint(api_recent_dates_bp, url_prefix='/recent_dates')
+app.register_blueprint(api_ask_bp, url_prefix='/ask')
+app.register_blueprint(api_sql_bp, url_prefix='/sql')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
+    app.run(host="127.0.0.1", port=8000, debug=True)
 
