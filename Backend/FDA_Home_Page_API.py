dc0822e2f0ef757c233a2b46a0da13e149b2b355
Girish Chandra Saxena <130221358+Girish023@users.noreply.github.com>
2024-09-19 10:19:18 +0530

    Update FDA_Home_Page_API.py

--- Backend/FDA_Home_Page_API.py
+++ Backend/FDA_Home_Page_API.py
@@ -3,8 +3,13 @@ from flask_cors import CORS
 from flask import Blueprint, jsonify
 import requests
 
+# app = Flask(__name__)
+# CORS(app, origins=["http://localhost:3000"])
+
+# @app.route('/titles', methods=['GET'])
+
 api_home_bp = Blueprint('home', __name__)
-CORS(api_home_bp, origins=["https://fda.ravooka.com"])
+CORS(api_home_bp, origins=["http://localhost:3000"])
 
 @api_home_bp.route('/titles', methods=['GET'])
 def get_titles():
@@ -24,4 +29,41 @@ def get_titles():
     else:
         return jsonify({"error": "Failed to fetch data"}), 500
 
+# @app.route('/recent_changes/<int:title_number>', methods=['GET'])
+@api_home_bp.route('/recent_changes/<int:title_number>', methods=['GET'])
+def get_recent_changes(title_number):
+    url = f'https://www.ecfr.gov/api/versioner/v1/versions/title-{title_number}.json?issue_date%5Bgte%5D=2024-07-03'
+    response = requests.get(url)
 
+    if response.status_code == 200:
+        data = response.json()
+        if 'content_versions' in data and data['content_versions']:
+            sections = data['content_versions']
+            sorted_sections = sorted(sections, key=lambda x: x['amendment_date'], reverse=True)
+
+            grouped_sections = {}
+            for section in sorted_sections:
+                date = section['amendment_date']
+                if date not in grouped_sections:
+                    grouped_sections[date] = []
+                grouped_sections[date].append(section)
+
+            recent_changes = []
+            for date, sections in grouped_sections.items():
+                sections_list = []
+                for section in sections:
+                    sections_list.append({
+                        "part": section.get('part', '—'),
+                        "subpart": section.get('subpart', '—'),
+                        "identifier": section.get('identifier', '§ —'),
+                        "name": section.get('name', '')
+                    })
+                recent_changes.append({
+                    "date": date,
+                    "sections": sections_list
+                })
+            return jsonify(recent_changes)
+        else:
+            return jsonify({"message": "No recent changes found for Title {}".format(title_number)})
+    else:
+        return jsonify({"error": "Failed to fetch data"}), 500
